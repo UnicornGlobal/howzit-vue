97aa3c0f538e9aa857f2722df515873b374d09f3
Darryn Ten <darryn.ten@gmail.com>
2018-11-27 12:22:32 +0200

    Skeleton

---
+++ src/Howzit.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-for="(key, field) in form" :key="key">
+      {{ key }}
+      {{ field }}
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script>
+export default {
+  props: {
+    form: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
